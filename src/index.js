bbbeb20c6784c38a3a0b63d336a30ec7cf1817ef
Angel-Barena <angelbarena89@hotmail.com>
2018-10-09 13:20:48 -0500

    add blog source files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import promise from 'redux-promise'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div className="d-flex justify-content-center">
+        <Switch>
+          <Route path="/posts/new" component={PostsNew}/>
+          <Route path="/posts/:id" component={PostsShow}/>
+          <Route path="/" component={PostsIndex}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('#root'));
