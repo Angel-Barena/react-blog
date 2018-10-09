3cf274ffc48cf8e2a5d8bc98b29df9c10227a2ce
Angel-Barena <angelbarena89@hotmail.com>
2018-10-09 13:25:53 -0500

    Updates

---
+++ precache-manifest.f72a19e8bc99619e289dddd42dcc05b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d69fcb038933d04a3a6",
+    "url": "/react-blog/static/js/runtime~main.7d69fcb0.js"
+  },
+  {
+    "revision": "841654d830fc6d604e7d",
+    "url": "/react-blog/static/js/main.841654d8.chunk.js"
+  },
+  {
+    "revision": "ada44bc98cc98b32423d",
+    "url": "/react-blog/static/js/1.ada44bc9.chunk.js"
+  },
+  {
+    "revision": "ada44bc98cc98b32423d",
+    "url": "/react-blog/static/css/1.50c2496c.chunk.css"
+  },
+  {
+    "revision": "69ec49fa532a460a4f344f1ade50b449",
+    "url": "/react-blog/index.html"
+  }
+];
